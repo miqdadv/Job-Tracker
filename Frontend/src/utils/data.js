49ba8735410f7ed727a8165adfdb75a9218d87c7
miqdadv <100848820+miqdadv@users.noreply.github.com>
2026-01-16 18:02:29 +0530

    fix backend url

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,5 +1,7 @@
-export const USER_API_ENDPOINT = "http://localhost:5001/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:5001/api/job";
-export const TRACKER_API_ENDPOINT = "http://localhost:5001/api/tracker";
-export const COMPANY_API_ENDPOINT = "http://localhost:5001/api/company";
-export const APPLICATION_API_ENDPOINT = "http://localhost:5001/api/application";
+export const BACKEND_URI = import.meta.env.VITE_BACKEND_URI || "http://localhost:5001";
+
+export const USER_API_ENDPOINT = `${BACKEND_URI}/api/user`;
+export const JOB_API_ENDPOINT = `${BACKEND_URI}/api/job`;
+export const TRACKER_API_ENDPOINT = `${BACKEND_URI}/api/tracker`;
+export const COMPANY_API_ENDPOINT = `${BACKEND_URI}/api/company`;
+export const APPLICATION_API_ENDPOINT = `${BACKEND_URI}/api/application`;
